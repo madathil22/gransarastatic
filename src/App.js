1950529c37a4e9c3757b9940f19230528ca22fc1
tobin22@gmail.com <tobin22@gmail.com>
2022-06-03 02:12:35 -0400

    changes4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Abraham Family Vacations</h1>
+
+      <header>
+        <h1>Abraham Family Vacations</h1>
+      </header>
       <table width="100%" height="100%" >
         <tr>
           <td>
@@ -16,6 +19,17 @@ function App() {
           </td>
           <td>
             <YoutubeEmbed embedId="vbJmyAIk85E" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <YoutubeEmbed embedId="bjQFcoeUGNY" />
+          </td>
+          <td>
+            <YoutubeEmbed embedId="t-8jfxnuA8s" />
+          </td>
+          <td>
+            <YoutubeEmbed embedId="FclsgqHceyM" />
           </td>
         </tr>
       </table>
